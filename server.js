37f9f00a1fe93093226bb8d717d75f2f530e33ab
unknown <tranthuongpro99@gmail.com>
2022-03-27 15:01:42 +0700

    push heroku

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path');
+app.use('/public', express.static( path.join(__dirname ,'/public')))
+
+
+app.get('/', (req, res , next ) => {
+
+    var pathFile = path.join(__dirname , 'home.html')
+    res.sendFile(pathFile)
+
+
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
