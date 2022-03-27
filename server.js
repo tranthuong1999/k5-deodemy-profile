e2499276aa2ec7d81387e8b9aa466c6b60564388
unknown <tranthuongpro99@gmail.com>
2022-03-27 22:43:34 +0700

    add page home

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ app.use('/public', express.static( path.join(__dirname ,'/public')))
 
 
 app.get('/', (req, res , next ) => {
-
     var pathFile = path.join(__dirname , 'home.html')
     res.sendFile(pathFile)
-
-
   })
+  app.get('/home', (req, res , next ) => {
+    res.json("home")
   
+  })
   app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port `)
   })
